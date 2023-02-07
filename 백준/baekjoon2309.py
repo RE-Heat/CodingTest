b4d9c244e6308452dc28f003acdf51b3b36d5329
정재열(RE-Heat) <111586909+RE-Heat@users.noreply.github.com>
2023-02-07 11:08:27 +0900

    백준 2309 - 일곱 난쟁이
    파이썬으로 처음 풀어본 초안

---
+++ 백준/baekjoon2309.py
@@ -0,0 +1,29 @@
+#백준2309 일곱난쟁이
+
+dwarfs = []
+
+#입력받은 값 리스트에 넣기
+for _ in range(9):
+  dwarfs.append(int(input()))
+
+#list 총합
+sumDwarfs = sum(dwarfs)
+
+#notDwarf1 notDwarf2 빼기
+notDwarf1 = 0
+notDwarf2 = 0
+
+for i in range(0, 8):
+  for j in range(1, 9):
+    a = sumDwarfs - dwarfs[i] - dwarfs[j]
+    if (a == 100):
+      notDwarf1 = dwarfs[i]
+      notDwarf2 = dwarfs[j]
+      break
+
+dwarfs.remove(notDwarf1)
+dwarfs.remove(notDwarf2)
+
+a = sorted(dwarfs)
+for i in a:
+  print(i)
