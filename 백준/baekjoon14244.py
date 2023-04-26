4495e5f1079d06d2afe43a98d7bf853956a759a7
정재열(RE-Heat) <111586909+RE-Heat@users.noreply.github.com>
2023-04-26 16:33:44 +0900

    백준 14244번 트리 만들기

---
+++ 백준/baekjoon14244.py
@@ -0,0 +1,15 @@
+#백준 14244번 트리 만들기
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+if m == 2:
+  for i in range(n - 1):
+    print(i, i + 1)
+else:
+  print(0, 1)
+  for i in range(2, m + 1):
+    print(1, i)
+  for j in range(m, n - 1):
+    print(j, j + 1)
