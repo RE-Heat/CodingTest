4668ff3e877326d42b3e0934c939ad15b4937fa6
리히트(RE-Heat) <111586909+RE-Heat@users.noreply.github.com>
2023-08-14 20:59:30 +0900

    백준 2839번 설탕배달

---
+++ 백준/baekjoon2839.py
@@ -0,0 +1,16 @@
+#백준 2839번 설탕배달
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+ans = 0
+
+while N >= 0:
+  if N % 5 == 0:
+    ans += N // 5
+    print(ans)
+    break
+  N -=3
+  ans +=1
+else:
+  print(-1)
